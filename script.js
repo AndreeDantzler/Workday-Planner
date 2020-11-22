cd118ab4560a447076276b3ac33fbe6544636fb2
Andree Dantzler <andree.balayssac@icloud.com>
2020-11-22 00:17:19 -0500

    cleaning up, removing consoles and unncessary variables and if statemnt

--- script.js
+++ script.js
@@ -7,16 +7,12 @@ function setTime() {
 
 //setting initial data to either be nothing or to be what we fetched from localstorage
 
-var data = JSON.parse(localStorage.getItem('scheduler'))|| {};
-console.log(data);
-console.log(Object.entries(data));
+var data = JSON.parse(localStorage.getItem("scheduler")) || {};
 
 //selecting items from local storage and setting them back on html
-Object.entries(data).forEach(function([key,value]) {
-  console.log(`${key} ${value}`);
+Object.entries(data).forEach(function ([key, value]) {
   $(`.time-${key}`).val(value);
 });
-
 
 //what is each timeblock, each timeblock is the row, the hour of the timeblock needs to be
 //compared with the current hour. if the hour of the timeblock is greater than current time, then it becomes future
@@ -50,39 +46,30 @@ var possibleHour = [
   "11PM",
 ];
 var currentHour = moment().format("ha").toUpperCase();
-console.log(currentHour);
 
 //Need to define in poss hour the arrays for past time and future times as this is what will be used to assign class
 
 var pastTime = possibleHour.slice(0, possibleHour.indexOf(currentHour));
 var futureTime = possibleHour.slice(possibleHour.indexOf(currentHour) + 1);
-var element = document.getElementsByClassName(`.time-${currentHour}`);
 
 //if current time
 
-if (element) {
-  $(`.time-${currentHour}`).addClass("present");
-}
+$(`.time-${currentHour}`).addClass("present");
 
 //if future time
 
 futureTime.forEach(function (time) {
-  if (element) {
-    $(`.time-${time}`).addClass("future");
-  }
+  $(`.time-${time}`).addClass("future");
 });
 
 //if past time
 
 pastTime.forEach(function (time) {
-  if (element) {
-    $(`.time-${time}`).addClass("past");
-  }
+  $(`.time-${time}`).addClass("past");
 });
 
-//saving data from textarea
 
-//i need to create a variable that captures input from text area and time associated with it
+//creating a variable that captures input from text area and time associated with it
 //that combination will be data i will store
 
 $(".saveBtn").each(function () {
@@ -90,9 +77,6 @@ $(".saveBtn").each(function () {
     var value = $(this).prev().val();
     var key = $(this).attr("id");
     data[key] = value;
-    console.log(data);
     localStorage.setItem("scheduler", JSON.stringify(data));
   });
 });
-
-
